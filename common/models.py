e3a6d0f86bd940be20271a50e18ec74f06730de6
QueenCurry <yeawonjo@gmail.com>
2019-12-13 10:50:37 +0900

    Add Model Notice

--- common/models.py
+++ common/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Notice(models.Model):
+    boardId = models.IntegerField(primary_key=True)
+    num = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=False)
+    link = models.CharField(max_length=300)
