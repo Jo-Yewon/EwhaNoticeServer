970e1d1278992f7d08f8976f31a77edfc750a565
QueenCurry <yeawonjo@gmail.com>
2020-02-04 11:48:47 +0900

    feat: 게시판 크롤링 중 에러 발생시 로그 찍을 수 있도록 수정

--- crawler.py
+++ crawler.py
@@ -34,59 +34,62 @@ def get_first_notice(notices):
 def get_notice():
     boards = Board.objects.all()
     for board in boards:
-        # 1. get data from url
-        notices = get_notices_data(board.base_url + board.next_url)
-        latest_index, latest_num = get_first_notice(notices)
-        if latest_index == -1 or latest_num <= board.saved_latest:
-            continue
+        try:
+            # 1. get data from url
+            notices = get_notices_data(board.base_url + board.next_url)
+            latest_index, latest_num = get_first_notice(notices)
+            if latest_index == -1 or latest_num <= board.saved_latest:
+                continue
 
-        # 2. save new notices
-        if board.board_type == Board.COMMON:
-            for notice in notices[latest_index:]:
-                td_list = notice.find_all("td")
-                category = td_list[1].string.strip()
-                num = int(td_list[0].string)
-                if num <= board.saved_latest:
-                    break
-                if category != '입학' and category != '등록금' and category != '입찰':
+            # 2. save new notices
+            if board.board_type == Board.COMMON:
+                for notice in notices[latest_index:]:
+                    td_list = notice.find_all("td")
+                    category = td_list[1].string.strip()
+                    num = int(td_list[0].string)
+                    if num <= board.saved_latest:
+                        break
+                    if category != '입학' and category != '등록금' and category != '입찰':
+                        Notice(board_category=board.board_category,
+                               board_id=board.board_id,
+                               num=num,
+                               title=td_list[2].a.string.strip(),
+                               category=category,
+                               date=td_list[3].string.strip(),
+                               url=board.base_url + td_list[2].a.attrs['href']).save()
+
+            elif board.board_type == Board.CATEGORY:
+                for notice in notices[latest_index:]:
+                    td_list = notice.find_all("td")
+                    num = int(td_list[0].string)
+                    if num <= board.saved_latest:
+                        break
                     Notice(board_category=board.board_category,
                            board_id=board.board_id,
                            num=num,
-                           title=td_list[2].a.string.strip(),
-                           category=category,
-                           date=td_list[3].string.strip(),
+                           title=td_list[2].text,
+                           category=td_list[1].string,
+                           date=td_list[4].text.strip(),
                            url=board.base_url + td_list[2].a.attrs['href']).save()
 
-        elif board.board_type == Board.CATEGORY:
-            for notice in notices[latest_index:]:
-                td_list = notice.find_all("td")
-                num = int(td_list[0].string)
-                if num <= board.saved_latest:
-                    break
-                Notice(board_category=board.board_category,
-                       board_id=board.board_id,
-                       num=num,
-                       title=td_list[2].text,
-                       category=td_list[1].string,
-                       date=td_list[4].text.strip(),
-                       url=board.base_url + td_list[2].a.attrs['href']).save()
+            elif board.board_type == Board.NON_CATEGORY:
+                for notice in notices[latest_index:]:
+                    td_list = notice.find_all("td")
+                    num = int(td_list[0].string)
+                    if num <= board.saved_latest:
+                        break
+                    Notice(board_category=board.board_category,
+                           board_id=board.board_id,
+                           num=num,
+                           title=td_list[1].a.text.strip(),
+                           category=None,
+                           date=td_list[3].text.strip(),
+                           url=board.base_url + td_list[1].a.attrs['href']).save()
 
-        elif board.board_type == Board.NON_CATEGORY:
-            for notice in notices[latest_index:]:
-                td_list = notice.find_all("td")
-                num = int(td_list[0].string)
-                if num <= board.saved_latest:
-                    break
-                Notice(board_category=board.board_category,
-                       board_id=board.board_id,
-                       num=num,
-                       title=td_list[1].a.text.strip(),
-                       category=None,
-                       date=td_list[3].text.strip(),
-                       url=board.base_url + td_list[1].a.attrs['href']).save()
-
-        # 3. update latest
-        board.update_latest(latest_num)
+            # 3. update latest
+            board.update_latest(latest_num)
+        except:
+            print(board.title+"을 크롤링하는 중에 에러 발생")
 
 
 if __name__ == '__main__':
