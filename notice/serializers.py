d15a002bda2f81207a2eda86f86020e11cc9995c
Jo-Yewon <yeawonjo@gmail.com>
2020-02-19 10:45:15 +0900

    feat: GET API 'board_id' -> url
    - board_id를 쿼리하면 url을 반환해주는 기능 및 endpoint

--- notice/serializers.py
+++ notice/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Notice
+from .models import Notice, Board
 
 
 class NoticeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Notice
         fields = ['num', 'title', 'category', 'date', 'url']
+
+
+class BoardSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['base_url', 'next_url']
